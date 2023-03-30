c24a751ac851a9d42a23f6c356cd13117f30c99d
keya006 <keyanelson006@yahoo.com>
2023-03-29 17:17:24 -0700

    new

--- cs_phase2_database_team#.sql
+++ cs_phase2_database_team#.sql
@@ -1,3 +1,44 @@
 -- Table structure for table location
 DROP TABLE IF EXISTS location;
 CREATE TABLE location (
+	locationID char(50) NOT NULL,
+	PRIMARY KEY (locationID)
+);
+
+-- Dumping data for table location
+INSERT INTO location VALUES
+('plane_1'),
+('plane_11'),
+('plane_15'),
+('plane_2'),
+('plane_4'),
+('plane_7'),
+('plane_8'),
+('plane_9'),
+('plane_1'),
+('plane_10'),
+('plane_11'),
+('plane_13'),
+('plane_14'),
+('plane_15'),
+('plane_17'),
+('plane_18'),
+('plane_2'),
+('plane_3'),
+('plane_4'),
+('plane_5'),
+('plane_7'),
+('plane_9');
+
+-- Creating table for person
+DROP TABLE IF EXISTS person;
+CREATE TABLE person (
+	personID char(50) NOT NULL,
+	first_name char(50) NOT NULL,
+	last_name char(50),
+	personlocationID char (50),
+	PRIMARY KEY (personID),
+	CONSTRAINT fk1 FOREIGN KEY (persolocationID) REFERENCES locationID
+);
+
+
